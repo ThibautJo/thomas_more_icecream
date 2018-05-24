b5eae48c22698b1a0607731d7740b4244d5bc305
Jorne Lambrechts <jornelambrechts@hotmail.com>
2018-05-24 11:03:58 +0200

    module file

--- src/Form/IceCreamForm.php
+++ src/Form/IceCreamForm.php
@@ -10,9 +10,22 @@ namespace Drupal\thomas_more_icecream\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-
+use Drupal\Core\State\StateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class IceCreamForm extends FormBase {
+
+  protected $state;
+
+  public function __construct(StateInterface $state) {
+    $this->state = $state;
+  }
+
+  public static function create(ContainerInterface $container){
+    return new static (
+        $container->get('state')
+    );
+  }
 
   public function getFormId() {
     return 'thomas_more_icecream_icecream_form';
@@ -22,7 +35,13 @@ class IceCreamForm extends FormBase {
     $form['keuzes'] = [
       '#type' => 'radios',
       '#title' => 'Maak uw keuze',
+      '#options' => $this->state->get('thomas_more_icecream.opties')
+    ];
 
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => 'Geef keuze door',
+      '#button_type' => 'primary'
     ];
   }
 
