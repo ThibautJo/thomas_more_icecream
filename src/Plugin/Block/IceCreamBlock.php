a8f5d1c79ac58a3a4e0b9d7a226dfd830795c216
Jorne Lambrechts <jornelambrechts@hotmail.com>
2018-05-24 10:01:05 +0200

    aanpassen IceCreamBlock

--- src/Plugin/Block/IceCreamBlock.php
+++ src/Plugin/Block/IceCreamBlock.php
@@ -3,17 +3,17 @@
 namespace Drupal\thomas_more_icecream\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+/*use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\State\StateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\thomas_more_social_media\ClickManager;
+use Drupal\thomas_more_social_media\ClickManager;*/
 
 /**
  * Defines a social menu block
  *
  * @Block(
- *   id = "thomas_more_social_media_block",
- *   admin_label = @Translation("Social media"),
+ *   id = "thomas_more_icecream_block",
+ *   admin_label = @Translation("Ice cream"),
  *  )
  */
 
